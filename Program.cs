d7d05b04d4a53127a799b3c51e90e419673cc9d6
TheSavior <github@eli-white.com>
2013-04-02 10:24:47 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardHistogram
+{
+    // Rules of the game
+    // Flip over a card
+    // if the card flipped and the one 4 before it are the same number
+    // discard all 4 cards
+    // if they are the same suit, but different number
+    // discard the two cards in the middle
+    // otherwise flip another card
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            int[] left = new int[52];
+            int games = 1000000;
+            // Play the game a bunch of times times
+            for (int i = 0; i < games; i++)
+            {
+                var cardsLeft = PlayGame(random);
+                left[cardsLeft]++;
+            }
+
+            using (StreamWriter file = new StreamWriter(@"histogram.txt"))
+            {
+                for (int i = 0; i < left.Length; i+=2)
+                {
+                    file.WriteLine((i + "\t" + left[i] / (float)games));
+                }
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        static int PlayGame(Random rand)
+        {
+            var deck = new Deck();
+            var cards = deck.getShuffled(rand);
+            
+            int index1 = 0;
+            int index2 = 3;
+
+            while (index2 < cards.Count)
+            {
+                if (cards[index1].value == cards[index2].value)
+                {
+                    cards.RemoveRange(index1, 4);
+
+                    // naive approach, restart our indexes
+                    index1 = 0;
+                    index2 = 4;
+                }
+                else if (cards[index1].suit == cards[index2].suit)
+                {
+                    cards.RemoveRange(index1 + 1, 2);
+
+                    // naive approach, restart our indexes
+                    index1 = 0;
+                    index2 = 4;
+                }
+                index1++;
+                index2++;
+            }
+
+            return cards.Count;
+        }
+    }
+
+    class Deck
+    {
+        private List<Card> deck;
+
+        public Deck() {
+            // Build our deck
+
+            List<Card> cards = new List<Card>();
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach(Value value in Enum.GetValues(typeof(Value)))
+                {
+                    cards.Add(new Card(suit, value));
+                }
+            }
+
+            deck = cards;
+        }
+
+        public List<Card> getShuffled(Random rand)
+        {
+            var cards = new List<Card>();
+
+            while (deck.Count > 0)
+            {
+                int index = rand.Next(deck.Count);
+                cards.Add(deck[index]);
+                deck.RemoveAt(index);
+            }
+
+            return cards;
+        }
+    }
+
+    class Card
+    {
+        public Suit suit { get; private set; }
+        public Value value { get; private set; }
+
+        public Card(Suit suit, Value value) {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return value + " of " + suit;
+        }
+    }
+
+    enum Suit
+    {
+        Heart, Spade, Diamond, Club
+    }
+
+    enum Value
+    {
+       Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+This project calculates a histogram to show the percent chance of ending a game of http://en.wikipedia.org/wiki/One-Handed_Solitaire_(Solitaire) with a set number of cards left.
+
+Resulting histogram after 1 Million games is:
+
+    0	0
+    2	0.017672
+    4	0.133068
+    6	0.21818
+    8	0.170906
+    10	0.129927
+    12	0.095439
+    14	0.069367
+    16	0.050922
+    18	0.036522
+    20	0.025942
+    22	0.018137
+    24	0.012458
+    26	0.008403
+    28	0.005392
+    30	0.003371
+    32	0.001967
+    34	0.001175
+    36	0.00066
+    38	0.000281
+    40	0.000132
+    42	5.50E-05
+    44	1.70E-05
+    46	4.00E-06
+    48	2.00E-06
+    50	1.00E-06
